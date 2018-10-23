87daa578f3df3f73e10036a310cdcc454b754365
Shaundra <s.m.crittenden@gmail.com>
2018-10-23 23:55:51 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,5 +3,5 @@ def display_rainbow(colors)
     colors.each do |color|
       color_string << " #{color[0][0].upcase}: #{color},"
     end
-    puts color_string.chomps.chop
+    puts color_string.strip.chop
 end
